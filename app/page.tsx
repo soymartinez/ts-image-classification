f11c90b7ce1d70fc4367d49567050963905870d2
omgismartinez <martnzomg@gmail.com>
2023-01-17 21:20:39 -0600

    🌌 refactor (page): test model

--- app/page.tsx
+++ app/page.tsx
@@ -1,90 +1,50 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client"
 
-const inter = Inter({ subsets: ['latin'] })
+import styles from './page.module.css'
+import { loadModel } from '@/utils/model'
+import { useState } from 'react'
+
+interface Data {
+  className: string
+  probability: number
+}
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [image, setImage] = useState<any>(null)
+  const [data, setData] = useState<Data[]>([])
+
+  const handleload = async (e: any) => {
+    e.preventDefault()
+    const element = e.target.files[0]
+
+    if (!element) return
+
+    setIsLoading(true)
+    const reader = new FileReader()
+    reader.readAsDataURL(element)
+    reader.onload = () => {
+      setImage(reader.result)
+    }
+
+    const img = document.getElementById('selected-image')
+    const data = await loadModel(img)
+
+    if (data) setData(data)
+    setIsLoading(false)
+  }
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={styles.content}>
+        <img id='selected-image' className={styles.selectedImage} src={image} width={200} height={200} />
+        <input onChange={(e) => handleload(e)} type='file' name='image' accept='image/*' disabled={isLoading} />
+        <div className={styles.resultsContent}>
+          {data && data.map(({ className, probability }) => (
+            <div key={className} className={styles.results}>
+              <p>{className}</p> <p className={styles.divider}>-</p> <p>{probability}</p>
+            </div>
+          ))}
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
